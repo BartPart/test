4221df1a460cb9ce57a3605842fc254f55466ded
Mateo Toquica A. <mateotoquica@gmail.com>
2015-04-19 11:54:11 -0500

    para animación.
    en la carpeta de scripts hay un scrip nuevo para para parar la animacion
    del dado, reconoce el click, pero no para la animación

---
+++ GameStair/Assets/Scripts/StopDice.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopDice : MonoBehaviour {
+
+	// Declare variables
+	private bool isHeld;
+	private int clicks;
+
+	// Use this for initialization
+	void Start ()
+	{
+		clicks = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (isHeld)
+		{
+			OnMouseDown();
+		}else if (clicks > 1)
+		{
+			OnMouseUp();   
+		}
+	}
+	
+	// If player clicks egg, it is picked up
+	void OnMouseDown ()
+	{      
+		Debug.Log("You're holding the egg!");
+		clicks ++;
+	}
+	
+	void OnMouseUp ()
+	{
+		isHeld = false;
+		Debug.Log("You released the egg!");
+		animation.stop();
+	}
+
+}
