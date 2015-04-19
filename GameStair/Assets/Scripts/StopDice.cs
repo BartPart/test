1d8a4c2778dbbe3eca57681d0528ea9ca42f2093
Mateo Toquica A. <mateotoquica@gmail.com>
2015-04-19 17:14:19 -0500

    codigo de captura de click optimizado

--- GameStair/Assets/Scripts/StopDice.cs
+++ GameStair/Assets/Scripts/StopDice.cs
@@ -16,27 +16,19 @@ public class StopDice : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		if (isHeld)
-		{
-			OnMouseDown();
-		}else if (clicks > 1)
+		if (clicks > 1)
 		{
 			OnMouseUp();   
 		}
 	}
 	
-	// If player clicks egg, it is picked up
-	void OnMouseDown ()
-	{      
-		Debug.Log("You're holding the egg!");
-		clicks ++;
-	}
+
 	
 	void OnMouseUp ()
 	{
 		isHeld = false;
-		Debug.Log("You released the egg!");
-		animation.stop();
+		Debug.Log("Usted toco el dado!");
+		//animation.stop();
 	}
 
 }
