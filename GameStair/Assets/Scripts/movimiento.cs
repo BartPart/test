fe21c9cfb9de48d83e1ca3ab54af15ca0936b02d
Mateo Toquica A <mateotoquica@gmail.com>
2015-05-11 14:44:22 -0500

    movimiento de nave completo
    adicionalmente tiene unas caricaturas en la escena del juego. También
    tiene otra opción en el menu para crear las preguntas.

--- GameStair/Assets/Scripts/movimiento.cs
+++ GameStair/Assets/Scripts/movimiento.cs
@@ -3,40 +3,89 @@ using System.Collections;
 
 public class movimiento : MonoBehaviour {
 
-
+	public Animator anim;
+	public SpriteRenderer animacion;
 	public Rigidbody2D rigit;
+
+
 	private StopDice stopdado;
 	private Vector2 vector2;
+
 
 
 
 	// Use this for initialization
 	void Start () {
 		rigit = GetComponent<Rigidbody2D>();
-		stopdado = GetComponent<StopDice>();
-	
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		int valor=stopdado.obtenerValor();
 
-		if (valor != 0) {
+		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+		{
+	
 
-			Debug.Log("entro");
-			moverNave();
-
+			startDice();
+			int valor = obtenerValor();
+			moverNave(valor);
 		}
 
-		
 	}
-	public void moverNave()
+	public void moverNave(int valor)
 	{
-		//vector2 = new Vector2 (valor, 0);
+		int valorMovimiento = valor + valor + valor+ valor+ valor+ valor;
+		vector2 = new Vector2 (valorMovimiento, 0);
 		
-		//rigit.MovePosition (rigit.position + vector2);
-		//rigit.transform.position = vector2;
+		rigit.MovePosition (rigit.position + vector2);
+
+	}
+	public int obtenerValor()
+	{
+		if (anim.speed == 0) {
+			
+			if (animacion.sprite.name.Equals ("dado sf_0")) {
+				return 1;
+			}
+			if (animacion.sprite.name.Equals ("dado sf_1")) {
+				return 2;
+				
+			}
+			if (animacion.sprite.name.Equals ("dado sf_2")) {
+				return 3;
+				
+			}
+			if (animacion.sprite.name.Equals ("dado sf_3")) {
+				return 4;
+				
+			}
+			if (animacion.sprite.name.Equals ("dado sf_4")) {
+				return 5;
+				
+			}
+			if (animacion.sprite.name.Equals ("dado sf_5")) {
+				return 6;
+				
+			}
+			return 0;
+		} else {
+			return 0;
+		}
+	}
+	public void startDice(){
+		
+		
+		
+		if (anim.speed == 1) {
+			
+			anim.speed = 0;
+			
+		} else {
+			
+			anim.speed = 1;
+		}
 	}
 }
 
